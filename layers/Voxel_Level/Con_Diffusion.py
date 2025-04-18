e37ff42ae001aede25b5e89e2a78214a0206e4f0
Hyungtae Lim <shapelim@mit.edu>
2025-04-18 10:25:44 -0400

    Set NotImplemtedError and apply pre-commit

--- layers/Voxel_Level/Con_Diffusion.py
+++ layers/Voxel_Level/Con_Diffusion.py
@@ -1,10 +1,11 @@
+from inspect import isfunction
+
+import numpy as np
 import torch
 import torch.nn.functional as F
-import numpy as np
-import math
-from inspect import isfunction
+
 from layers.Voxel_Level.denoise import Denoise
-from utils.loss import *
+
 """
 Based in part on: https://github.com/lucidrains/denoising-diffusion-pytorch/blob/5989f4c77eafcdc6be0fb4739f0f277a6dd7f7d8/denoising_diffusion_pytorch/denoising_diffusion_pytorch.py#L281
 """
@@ -45,8 +46,8 @@ def log_categorical(log_x_start, log_prob):
 
 
 def index_to_log_onehot(x, num_classes):
-    assert x.max().item() < num_classes, f'Error: {x.max().item()} >= {num_classes}'
-    
+    assert x.max().item() < num_classes, f"Error: {x.max().item()} >= {num_classes}"
+
     x_onehot = F.one_hot(x, num_classes)
     permute_order = (0, -1) + tuple(range(1, len(x.size())))
     x_onehot = x_onehot.permute(permute_order)
@@ -58,7 +59,7 @@ def log_onehot_to_index(log_x):
     return log_x.argmax(1)
 
 
-def cosine_beta_schedule(timesteps, s = 0.008):
+def cosine_beta_schedule(timesteps, s=0.008):
     """
     cosine schedule
     as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
@@ -67,33 +68,47 @@ def cosine_beta_schedule(timesteps, s = 0.008):
     x = np.linspace(0, steps, steps)
     alphas_cumprod = np.cos(((x / steps) + s) / (1 + s) * np.pi * 0.5) ** 2
     alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
-    alphas = (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    alphas = alphas_cumprod[1:] / alphas_cumprod[:-1]
 
-    alphas = np.clip(alphas, a_min=0.001, a_max=1.)
+    alphas = np.clip(alphas, a_min=0.001, a_max=1.0)
     alphas = np.sqrt(alphas)
 
     return alphas
 
+
 class Con_Diffusion(torch.nn.Module):
-    def __init__(self, args, multi_criterion,auxiliary_loss_weight=0.05, adaptive_auxiliary_loss=True):
+    def __init__(
+        self,
+        args,
+        multi_criterion,
+        auxiliary_loss_weight=0.05,
+        adaptive_auxiliary_loss=True,
+    ):
         super(Con_Diffusion, self).__init__()
 
-        #self._denoise_fn = SSCNet(num_classes=args.num_classes*50, num_steps=args.diffusion_steps)
+        # self._denoise_fn = SSCNet(num_classes=args.num_classes*50, num_steps=args.diffusion_steps)
         self.args = args
         self.num_classes = self.args.num_classes
         self.num_timesteps = self.args.diffusion_steps
         self.recon_loss = self.args.recon_loss
-        if args.dataset == 'carla':
-            self._denoise_fn = Denoise(args= self.args,  num_class = self.num_classes)
-        elif args.dataset=='kitti':
-            self._denoise_fn = Denoise(args= self.args,  num_class = self.num_classes, init_size=16)
+        if args.dataset == "carla":
+            self._denoise_fn = Denoise(args=self.args, num_class=self.num_classes)
+        elif args.dataset == "kitti":
+            self._denoise_fn = Denoise(
+                args=self.args, num_class=self.num_classes, init_size=16
+            )
+        elif args.dataset == "3dfront":
+            self._denoise_fn = Denoise(args=self.args, num_class=self.num_classes)
+        else:
+            raise NotImplementedError("Unsupported dataset has come.")
+
         self.auxiliary_loss_weight = auxiliary_loss_weight
         self.adaptive_auxiliary_loss = adaptive_auxiliary_loss
 
         self.multi_criterion = multi_criterion
 
-        alphas = cosine_beta_schedule(self.num_timesteps )
-        alphas = torch.tensor(alphas.astype('float64'))
+        alphas = cosine_beta_schedule(self.num_timesteps)
+        alphas = torch.tensor(alphas.astype("float64"))
 
         log_alpha = np.log(alphas)
         log_cumprod_alpha = np.cumsum(log_alpha)
@@ -101,19 +116,22 @@ class Con_Diffusion(torch.nn.Module):
         log_1_min_alpha = log_1_min_a(log_alpha)
         log_1_min_cumprod_alpha = log_1_min_a(log_cumprod_alpha)
 
-        assert log_add_exp(log_alpha, log_1_min_alpha).abs().sum().item() < 1.e-5
-        assert log_add_exp(log_cumprod_alpha, log_1_min_cumprod_alpha).abs().sum().item() < 1e-5
-        assert (np.cumsum(log_alpha) - log_cumprod_alpha).abs().sum().item() < 1.e-5
+        assert log_add_exp(log_alpha, log_1_min_alpha).abs().sum().item() < 1.0e-5
+        assert (
+            log_add_exp(log_cumprod_alpha, log_1_min_cumprod_alpha).abs().sum().item()
+            < 1e-5
+        )
+        assert (np.cumsum(log_alpha) - log_cumprod_alpha).abs().sum().item() < 1.0e-5
 
         # Convert to float32 and register buffers.
-        self.register_buffer('log_alpha', log_alpha.float())
-        self.register_buffer('log_1_min_alpha', log_1_min_alpha.float())
-        self.register_buffer('log_cumprod_alpha', log_cumprod_alpha.float())
-        self.register_buffer('log_1_min_cumprod_alpha', log_1_min_cumprod_alpha.float())
+        self.register_buffer("log_alpha", log_alpha.float())
+        self.register_buffer("log_1_min_alpha", log_1_min_alpha.float())
+        self.register_buffer("log_cumprod_alpha", log_cumprod_alpha.float())
+        self.register_buffer("log_1_min_cumprod_alpha", log_1_min_cumprod_alpha.float())
 
-        self.register_buffer('Lt_history', torch.zeros(self.num_timesteps ))
-        self.register_buffer('Lt_count', torch.zeros(self.num_timesteps ))
-    
+        self.register_buffer("Lt_history", torch.zeros(self.num_timesteps))
+        self.register_buffer("Lt_count", torch.zeros(self.num_timesteps))
+
     def device(self):
         return self.denoise_fn.device
 
@@ -126,21 +144,22 @@ class Con_Diffusion(torch.nn.Module):
         log_1_min_alpha_t = extract(self.log_1_min_alpha, t, log_x_t.shape)
 
         # alpha_t * E[xt] + (1 - alpha_t) 1 / K
-        
+
         log_probs = log_add_exp(
-            log_x_t + log_alpha_t,
-            log_1_min_alpha_t - np.log(self.num_classes)
+            log_x_t + log_alpha_t, log_1_min_alpha_t - np.log(self.num_classes)
         )
 
         return log_probs
 
     def q_pred(self, log_x_start, t):
         log_cumprod_alpha_t = extract(self.log_cumprod_alpha, t, log_x_start.shape)
-        log_1_min_cumprod_alpha = extract(self.log_1_min_cumprod_alpha, t, log_x_start.shape)
+        log_1_min_cumprod_alpha = extract(
+            self.log_1_min_cumprod_alpha, t, log_x_start.shape
+        )
 
         log_probs = log_add_exp(
             log_x_start + log_cumprod_alpha_t,
-            log_1_min_cumprod_alpha - np.log(self.num_classes)
+            log_1_min_cumprod_alpha - np.log(self.num_classes),
         )
 
         return log_probs
@@ -170,7 +189,9 @@ class Con_Diffusion(torch.nn.Module):
         # Not very easy to see why this is true. But it is :)
         unnormed_logprobs = log_EV_qxtmin_x0 + self.q_pred_one_timestep(log_x_t, t)
 
-        log_EV_xtmin_given_xt_given_xstart = unnormed_logprobs - torch.logsumexp(unnormed_logprobs, dim=1, keepdim=True)
+        log_EV_xtmin_given_xt_given_xstart = unnormed_logprobs - torch.logsumexp(
+            unnormed_logprobs, dim=1, keepdim=True
+        )
 
         return log_EV_xtmin_given_xt_given_xstart
 
@@ -202,10 +223,10 @@ class Con_Diffusion(torch.nn.Module):
         kl_prior = self.multinomial_kl(log_qxT_prob, log_half_prob)
         return sum_except_batch(kl_prior)
 
-    def sample_time(self, b, device, method='uniform'):
-        if method == 'importance':
+    def sample_time(self, b, device, method="uniform"):
+        if method == "importance":
             if not (self.Lt_count > 10).all():
-                return self.sample_time(b, device, method='uniform')
+                return self.sample_time(b, device, method="uniform")
 
             Lt_sqrt = torch.sqrt(self.Lt_history + 1e-10) + 0.0001
             Lt_sqrt[0] = Lt_sqrt[1]  # Overwrite decoder term with L1.
@@ -217,7 +238,7 @@ class Con_Diffusion(torch.nn.Module):
 
             return t, pt
 
-        elif method == 'uniform':
+        elif method == "uniform":
             t = torch.randint(0, self.num_timesteps, (b,), device=device).long()
 
             pt = torch.ones_like(t).float() / self.num_timesteps
@@ -227,11 +248,13 @@ class Con_Diffusion(torch.nn.Module):
 
     def forward(self, x, voxel_input):
         b, device = x.size(0), x.device
-        self.shape = x.size()[1:]        
-        t, pt = self.sample_time(b, device, 'importance')
+        self.shape = x.size()[1:]
+        t, pt = self.sample_time(b, device, "importance")
 
         log_x_start = index_to_log_onehot(x, self.num_classes)
-        log_x_t = self.q_sample(log_x_start, t) # log_x_t : (batch, #class, 128, 128, 8)
+        log_x_t = self.q_sample(
+            log_x_start, t
+        )  # log_x_t : (batch, #class, 128, 128, 8)
 
         log_true_prob = self.q_posterior(log_x_start=log_x_start, log_x_t=log_x_t, t=t)
         log_model_prob, log_x0_recon = self.p_pred(log_x=log_x_t, t=t, cond=voxel_input)
@@ -243,8 +266,8 @@ class Con_Diffusion(torch.nn.Module):
         decoder_nll = sum_except_batch(decoder_nll)
 
         mask = (t == torch.zeros_like(t)).float()
-        kl_loss = mask * decoder_nll + (1. - mask) * kl
-        
+        kl_loss = mask * decoder_nll + (1.0 - mask) * kl
+
         if self.training:
             Lt2 = kl_loss.pow(2)
             Lt2_prev = self.Lt_history.gather(dim=0, index=t)
@@ -257,48 +280,53 @@ class Con_Diffusion(torch.nn.Module):
         # Upweigh loss term of the kl
         loss = kl_loss / pt + kl_prior
 
-        kl_aux = self.multinomial_kl(log_x_start[:,:-1,:,:,:], log_x0_recon[:,:-1,:,:,:])
+        kl_aux = self.multinomial_kl(
+            log_x_start[:, :-1, :, :, :], log_x0_recon[:, :-1, :, :, :]
+        )
         kl_aux = sum_except_batch(kl_aux)
-        if self.recon_loss : 
+        if self.recon_loss:
             kl_aux += self.multi_criterion(log_x0_recon.exp(), x)
-            #kl_aux += lovasz_softmax(torch.nn.functional.softmax(log_x0_recon.exp(), dim=1), x)
+            # kl_aux += lovasz_softmax(torch.nn.functional.softmax(log_x0_recon.exp(), dim=1), x)
 
-        kl_aux_loss = mask * decoder_nll + (1. - mask) * kl_aux
+        kl_aux_loss = mask * decoder_nll + (1.0 - mask) * kl_aux
         if self.adaptive_auxiliary_loss:
-            addition_loss_weight = (1-t/self.num_timesteps) + 1.0
+            addition_loss_weight = (1 - t / self.num_timesteps) + 1.0
         else:
             addition_loss_weight = 1.0
 
         aux_loss = addition_loss_weight * self.auxiliary_loss_weight * kl_aux_loss / pt
-        
+
         loss += aux_loss
-        loss = -loss.sum() / (self.shape[0]*self.shape[1])
-        #loss += seg_loss
+        loss = -loss.sum() / (self.shape[0] * self.shape[1])
+        # loss += seg_loss
 
         return -loss
 
     def sample(self, voxel_input, intermediate=False):
         device = self.log_alpha.device
         self.shape = voxel_input.size()[1:]
-        uniform_logits = torch.zeros((self.args.batch_size, self.num_classes) + self.shape, device=device)
+        uniform_logits = torch.zeros(
+            (self.args.batch_size, self.num_classes) + self.shape, device=device
+        )
         log_z = self.log_sample_categorical(uniform_logits)
         diffusion = []
 
         for i in reversed(range(0, self.num_timesteps)):
-            print(f'Sample timestep {i:4d}', end='\r')
+            print(f"Sample timestep {i: 4d}", end="\r")
 
             t = torch.full((self.args.batch_size,), i, device=device, dtype=torch.long)
 
-            log_model_prob, log_x0_recon = self.p_pred(log_x=log_z, t=t, cond=voxel_input)
+            log_model_prob, log_x0_recon = self.p_pred(
+                log_x=log_z, t=t, cond=voxel_input
+            )
 
             log_z = self.log_sample_categorical(log_model_prob)
 
-            if i%10 ==0:
+            if i % 10 == 0:
                 diffusion.append(log_onehot_to_index(log_z))
 
         result = log_onehot_to_index(log_z)
-        if intermediate : 
+        if intermediate:
             return result, diffusion
-        else : 
+        else:
             return result
-
